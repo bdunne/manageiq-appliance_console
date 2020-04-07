0b5773b6031a61064a210b2620522f72fd5cd666
Brandon Dunne <brandondunne@hotmail.com>
2020-04-07 17:35:19 -0400

    simplify for external config first

--- lib/manageiq/appliance_console/queue_configuration.rb
+++ lib/manageiq/appliance_console/queue_configuration.rb
@@ -1,5 +1,4 @@
 require "pathname"
-require "linux_admin"
 
 module ManageIQ
   module ApplianceConsole
@@ -7,20 +6,7 @@ module ManageIQ
       include ApplianceConsole::Logging
       QUEUE_YML = ManageIQ::ApplianceConsole::RAILS_ROOT.join("config/queue.yml")
 
-      attr_accessor :disk, :encoding, :host, :local, :password, :port, :protocol, :username
-
-      def initialize(hash = {})
-        set_defaults
-        self.local = hash[:local]
-      end
-
-      def set_defaults
-        self.encoding  = "json"
-        self.host      = "localhost"
-        self.port      = 9092
-        self.protocol  = "Kafka"
-        self.username  = "root"
-      end
+      attr_accessor :host, :password, :port, :username
 
       def run_interactive
         ask_questions
@@ -41,115 +27,63 @@ module ManageIQ
         ask_for_queue_credentials
       end
 
-      def ask_for_queue_credentials(password_twice = true)
-        self.host     = ask_for_ip_or_hostname("queue hostname or IP address", host) if host.blank? || !local
-        self.port     = ask_for_integer("port number", nil, port) unless local
-        self.username = just_ask("username", username) unless local
+      def ask_for_queue_credentials
+        self.host     = ask_for_ip_or_hostname("queue hostname or IP address")
+        self.port     = ask_for_integer("port number", (1..65535), 9092)
+        self.username = just_ask("username")
         count = 0
         loop do
-          password1 = ask_for_password("queue password on #{host}", password)
-          # if they took the default, just bail
-          break if (password1 == password)
+          password1 = ask_for_password("queue password on #{host}")
 
           if password1.strip.length == 0
             say("\nPassword can not be empty, please try again")
             next
           end
-          if password_twice
-            password2 = ask_for_password("queue password again")
-            if password1 == password2
-              self.password = password1
-              break
-            elsif count > 0 # only reprompt password once
-              raise "passwords did not match"
-            else
-              count += 1
-              say("\nThe passwords did not match, please try again")
-            end
-          else
+
+          password2 = ask_for_password("queue password again")
+          if password1 == password2
             self.password = password1
             break
+          elsif count > 0 # only reprompt password once
+            raise "passwords did not match"
+          else
+            count += 1
+            say("\nThe passwords did not match, please try again")
           end
         end
       end
 
       def friendly_inspect
-        output =  "Host:     #{host}\n"
-        output << "Username: #{username}\n"
-        output << "Port:     #{port}\n" if port
-        output
+        <<~EOS
+          Host:     #{host}
+          Username: #{username}
+          Port:     #{port}
+        EOS
       end
 
       def activate
-        initialize_queue
         save
         true
       end
 
-      def initialize_queue
-        log_and_feedback(__method__) do
-          start_zookeeper
-          relabel_queue_dir
-          start_kafka
-          # create_queue_user
-        end
-      end
-
       private
 
-      def mount_point
-        "/var/lib/kafka"
-      end
-
-      def queue_mount_point?
-        LinuxAdmin::LogicalVolume.mount_point_exists?(mount_point.to_s)
-      end
-
-      def relabel_queue_dir
-        AwesomeSpawn.run!("/sbin/restorecon -R -v #{mount_point}")
-      end
-
-      def start_zookeeper
-        LinuxAdmin::Service.new("zookeeper").enable.start
-      end
-
-      def start_kafka
-        LinuxAdmin::Service.new("kafka").enable.start
-      end
-
-      # merge all the non specified setings
-      # for all the basic attributes, overwrite from this object (including blank values)
-      def merged_settings
-        merged = self.class.current
-        settings_hash.each do |k, v|
-          if v.present?
-            merged['production'][k] = v
-          else
-            merged['production'].delete(k)
-          end
-        end
-        merged
+      def settings_from_input
+        {
+          "production" => {
+            "hostname" => host,
+            "password" => password,
+            "port"     => port,
+            "username" => username
+          }
+        }
       end
 
       def save(settings = nil)
-        settings ||= merged_settings
-        do_save(settings)
-      end
+        settings ||= settings_from_input
 
-      def do_save(settings)
         require 'yaml'
         File.write(DB_YML, YAML.dump(settings))
-      end
-
-      def configure_ssl
-        cert_file = PostgresAdmin.data_directory.join("server.crt").to_s
-        key_file  = PostgresAdmin.data_directory.join("server.key").to_s
-        AwesomeSpawn.run!("/usr/bin/generate_miq_server_cert.sh", :env => {"NEW_CERT_FILE" => cert_file, "NEW_KEY_FILE"  => key_file})
-
-        FileUtils.chown("postgres", "postgres", cert_file)
-        FileUtils.chown("postgres", "postgres", key_file)
-        FileUtils.chmod(0644, cert_file)
-        FileUtils.chmod(0600, key_file)
       end
     end
   end
